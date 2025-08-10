2c0b21cec1287283174d45d961266df8934ba56d
Danilo Casim Jr. <danilocasim174@gmail.com>
2025-08-10 12:45:08 +0800

    feat: add Skills functionality that can edit the skills and update the changes in real-time

---
+++ src/components/Skills.jsx
@@ -0,0 +1,35 @@
+import { Input } from "./Input";
+
+export function Skills({ updateAllData, allData }) {
+  const newAllData = { ...allData };
+
+  const updateSkillsInfo = (prop, value) => {
+    newAllData.skills[prop] = value;
+    updateAllData(newAllData);
+  };
+
+  return (
+    <div>
+      <Input
+        label={"languages"}
+        value={allData.skills.languages}
+        onChange={(e) => updateSkillsInfo("languages", e.target.value)}
+      ></Input>
+      <Input
+        label={"frameworks"}
+        value={allData.skills.frameworks}
+        onChange={(e) => updateSkillsInfo("frameworks", e.target.value)}
+      ></Input>
+      <Input
+        label={"Developer Tools"}
+        value={allData.skills.devTools}
+        onChange={(e) => updateSkillsInfo("devTools", e.target.value)}
+      ></Input>
+      <Input
+        label={"libraries"}
+        value={allData.skills.libraries}
+        onChange={(e) => updateSkillsInfo("libraries", e.target.value)}
+      ></Input>
+    </div>
+  );
+}
