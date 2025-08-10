388316a7bbe8eb3018f6bf176aca4a53fb565de7
Danilo Casim Jr. <danilocasim174@gmail.com>
2025-08-10 12:44:09 +0800

    feat: add Experience component that has a edit, delete, add, prev, next to whole experience section and same functionality to added bullet responsibility

---
+++ src/components/Experience.jsx
@@ -0,0 +1,122 @@
+import { Input } from "./Input";
+import { useState } from "react";
+
+export function Experience({ updateAllData, allData }) {
+  const [currentIndex, setIndex] = useState(0);
+  const newAllData = { ...allData };
+
+  const incrementIndex = () => {
+    newAllData.experience = [
+      ...newAllData.experience,
+      {
+        company: "",
+        position: "",
+        location: "",
+        startWorking: "",
+        endWorking: "",
+        keyResponsibilities: [""],
+      },
+    ];
+    setIndex(currentIndex + 1);
+  };
+
+  const decrementIndex = () => {
+    setIndex(currentIndex - 1);
+  };
+
+  const deleteCurrentInfo = () => {
+    decrementIndex();
+    newAllData.experience.splice(currentIndex, 1);
+    updateAllData(newAllData);
+  };
+
+  const updateExperienceInfo = (prop, value) => {
+    newAllData.experience[currentIndex][prop] = value;
+    updateAllData(newAllData);
+  };
+
+  const updateResponsibility = (value, index) => {
+    newAllData.experience[currentIndex].keyResponsibilities[index] = value;
+    updateAllData(newAllData);
+  };
+
+  const addResponsibility = () => {
+    newAllData.experience[currentIndex].keyResponsibilities.push("");
+    updateAllData(newAllData);
+  };
+
+  const deleteResponsibility = (responsibilityIndex) => {
+    newAllData.experience[currentIndex].keyResponsibilities.splice(
+      responsibilityIndex,
+      1
+    );
+
+    updateAllData(newAllData);
+  };
+
+  return (
+    <div>
+      {newAllData.experience.length - 1 !== 0 && (
+        <button onClick={deleteCurrentInfo}>Delete</button>
+      )}
+      {0 < currentIndex && <button onClick={decrementIndex}>previous</button>}
+      {newAllData.experience.length - 1 > currentIndex && (
+        <button onClick={incrementIndex}>next</button>
+      )}
+      <Input
+        label={"company"}
+        value={allData.experience[currentIndex].company}
+        onChange={(e) => updateExperienceInfo("company", e.target.value)}
+      ></Input>
+      <Input
+        label={"position"}
+        value={allData.experience[currentIndex].position}
+        onChange={(e) => updateExperienceInfo("position", e.target.value)}
+      ></Input>
+      <Input
+        label={"location"}
+        value={allData.experience[currentIndex].location}
+        onChange={(e) => updateExperienceInfo("location", e.target.value)}
+      ></Input>
+      <Input
+        label={"Start Working"}
+        value={allData.experience[currentIndex].startWorking}
+        onChange={(e) => updateExperienceInfo("startWorking", e.target.value)}
+      ></Input>
+
+      <Input
+        label={"End Working"}
+        value={allData.experience[currentIndex].endWorking}
+        onChange={(e) => updateExperienceInfo("endWorking", e.target.value)}
+      ></Input>
+      <button onClick={addResponsibility}>Add Responsibility</button>
+      <>
+        {newAllData.experience[currentIndex].keyResponsibilities.map(
+          (responsibility, index) => {
+            return (
+              <>
+                <button onClick={deleteResponsibility}> delete</button>
+                <Input
+                  label={"Key Responsibility"}
+                  value={responsibility}
+                  onChange={(e) => updateResponsibility(e.target.value, index)}
+                ></Input>
+              </>
+            );
+          }
+        )}
+      </>
+
+      {newAllData.experience.length - 1 == currentIndex && (
+        <button
+          onClick={() => {
+            updateAllData(newAllData);
+            incrementIndex();
+          }}
+        >
+          Add
+        </button>
+      )}
+    </div>
+  );
+}
