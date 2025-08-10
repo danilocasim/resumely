6f3698ffe7454a4f5b78d2ba47d8bb8f50c4ce49
Danilo Casim Jr. <danilocasim174@gmail.com>
2025-08-10 12:46:47 +0800

    feat: move the initialState info for all of the information in a seperate file

---
+++ src/components/InitialInfo.jsx
@@ -0,0 +1,34 @@
+export const initialInfo = {
+  personalInfo: {
+    name: "Danilo Casim",
+    address: "adress",
+    email: "danilo@gmail",
+    number: "08982242",
+  },
+  education: [
+    {
+      school: "PLMUN",
+      degree: "LOL",
+      location: "Muntinlupa",
+      schoolStart: "2332-232-2",
+      schoolEnd: "232-45-4",
+    },
+  ],
+  experience: [
+    {
+      company: "Company 1",
+      position: "Software Engineer",
+      location: "Italy",
+      startWorking: "23254",
+      endWorking: "232343",
+      keyResponsibilities: ["responsibility 1", "responsibility 2"],
+    },
+  ],
+
+  skills: {
+    languages: "JS, JAVA",
+    frameworks: "Vue",
+    devTools: "Git, Github",
+    libraries: "Bootstrap, React Native, ReactJs",
+  },
+};
