53088c595189300529eec5dade3b49151222a79a
Danilo Casim Jr. <danilocasim174@gmail.com>
2025-08-12 01:53:21 +0800

    feat: change the rendering to render all of the data

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,47 +1,89 @@
 export function Resume({ info }) {
   return (
-    <>
-      <h1>{info.personalInfo.name}</h1>
-      <h1>{info.personalInfo.address}</h1>
-      <h1>{info.personalInfo.email}</h1>
-      <h1>{info.personalInfo.number}</h1>
+    <div className='resume' id='resume' key='resume'>
+      <div className='intro'>
+        <p className='title'>{info.personalInfo.name}</p>
+        <hr />
+        <ul>
+          <li>{info.personalInfo.address}</li>
+          <li>{info.personalInfo.email}</li>
+          <li>{info.personalInfo.number}</li>
+        </ul>
+      </div>
 
-      {info.education.map((data) => {
-        return (
-          <>
-            <h1>{data.school}</h1>
-            <h1>{data.degree}</h1>
-            <h1>{data.location}</h1>
-            <h1>{data.schoolStart}</h1>
-            <h1>{data.schoolEnd}</h1>
-          </>
-        );
-      })}
+      <div className='education'>
+        <p className='title'>Education</p>
+        {info.education.map((data) => {
+          return (
+            <div>
+              <div className='row'>
+                <p className='school'>{data.school}</p>
+                <p>{data.degree}</p>
+              </div>
+              <div className='row'>
+                <p>{data.location}</p>
+                <p>
+                  {data.schoolStart} - {data.schoolEnd}
+                </p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
 
-      {info.experience.map((data) => {
-        return (
-          <>
-            <h1>{data.company}</h1>
-            <h1>{data.position}</h1>
-            <h1>{data.location}</h1>
-            <h1>{data.startWorking}</h1>
-            <h1>{data.endWorking}</h1>
+      <div className='experience'>
+        <p className='title'>Experience</p>
+        {info.experience.map((data) => {
+          return (
+            <div className='job'>
+              <div className='row'>
+                <p className='bold'>{data.company}</p>
+                <p>{data.location}</p>
+              </div>
 
-            {data.keyResponsibilities.map((res) => {
-              return (
-                <>
-                  <h1>{res}</h1>
-                </>
-              );
-            })}
-          </>
-        );
-      })}
+              <div className='row'>
+                <p className='bold'>{data.position}</p>
+                <p>
+                  {data.startWorking} - {data.endWorking}
+                </p>
+              </div>
 
-      <h1>{info.skills.languages}</h1>
-      <h1>{info.skills.frameworks}</h1>
-      <h1>{info.skills.devTools}</h1>
-      <h1>{info.skills.libraries}</h1>
-    </>
+              <ul>
+                {data.keyResponsibilities.map((res) => {
+                  return (
+                    <>
+                      <li>{res}</li>
+                    </>
+                  );
+                })}
+              </ul>
+            </div>
+          );
+        })}
+      </div>
+
+      <div className='skills'>
+        <p className='title'>Skills</p>
+        <p>
+          <span className='bold'>Technical: </span>
+          {info.skills.languages}
+        </p>
+        <p>
+          {" "}
+          <span className='bold'>Frameworks: </span>
+          {info.skills.frameworks}
+        </p>
+        <p>
+          {" "}
+          <span className='bold'>Developer Tools: </span>
+          {info.skills.devTools}
+        </p>
+        <p>
+          {" "}
+          <span className='bold'>Libraries: </span>
+          {info.skills.libraries}
+        </p>
+      </div>
+    </div>
   );
 }
