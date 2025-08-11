48fbc3aa5fe543f3716a4ad17cfb8ea802ccd9e2
Danilo Casim Jr. <danilocasim174@gmail.com>
2025-08-12 01:39:47 +0800

    feat: render the Navigation Bar component to header

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import NavigationBar from "./components/Navigation.jsx";
+
+createRoot(document.querySelector("header")).render(
+  <StrictMode>
+    <NavigationBar />
+  </StrictMode>
+);
 
 createRoot(document.querySelector("main")).render(
   <StrictMode>
